f5b2b707e719a499ad9b42b2fe1526f3292fd6ab
Niall Kelly <nkelly@rumblelabs.com>
2014-09-26 12:13:32 +0100

    added objc bridging header and MagicalRecord as submodule

---
+++ SupportingFiles/CarpeDM-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  CarpeDM-Bridging-Header.h
+//  CarpeDM
+//
+//  Created by Niall Kelly on 26/09/2014.
+//  Copyright (c) 2014 Rumble Labs. All rights reserved.
+//
+
+#ifndef CarpeDM_CarpeDM_Bridging_Header_h
+#define CarpeDM_CarpeDM_Bridging_Header_h
+
+//#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "CoreData+MagicalRecord.h"
+
+#endif
